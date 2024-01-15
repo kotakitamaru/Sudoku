4f28a310c011ec9e567a725e5df469e01559ba15
Jisat <jisreserv@gmail.com>
2024-01-15 21:37:28 +0200

    Fixed bug where first initialization of sudoku were random data

--- src/lib/storesInitialization.ts
+++ src/lib/storesInitialization.ts
@@ -1,13 +1,20 @@
 import {browser} from "$app/environment";
 import {generateSudoku} from "$lib/sudokuGenerator";
 import {Difficult} from "$lib/types";
+import {difficult} from "$lib/stores";
 
 export function initInitialField() {
     if(browser) {
         const str = window.localStorage.getItem("initial_field");
         let res: number[][];
+        let diff: number|undefined;
+        difficult.subscribe((val)=>{
+            diff = val;
+        })
+        if(!diff)
+            throw new Error(":D");
         if (!str || str === "") {
-            res = generateSudoku(40);
+            res = generateSudoku(diff);
             window.localStorage.setItem("initial_field", JSON.stringify(res));
         } else
             res = JSON.parse(str);
@@ -20,7 +27,7 @@ export function initUserField() {
         const str = window.localStorage.getItem("user_field");
         let res: number[][];
         if (!str || str === "") {
-            res = generateSudoku(0);
+            res = generateSudoku(81);
             window.localStorage.setItem("user_field", JSON.stringify(res));
         } else
             res = JSON.parse(str);
